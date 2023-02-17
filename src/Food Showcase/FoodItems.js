a331f3d3335b93929f2e70ffcda25d3f00cfdac3
anaskhonprime <smlife202@gmail.com>
2023-02-17 21:37:47 +0900

    Get Started Firebase

--- src/Food Showcase/FoodItems.js
+++ src/Food Showcase/FoodItems.js
@@ -15,7 +15,7 @@ const Foods = () => {
             />
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">{key.name}</div>
-              <p className="text-gray-700 text-base">{key.content}</p>
+              <p className="text-gray-700 text-base">{key.price} Won</p>
             </div>
             <div className="px-6 py-4">
               <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
--- src/asset/data/food.js
+++ src/asset/data/food.js
@@ -4,6 +4,21 @@ const foodData = [
     content: "Here The Content",
     price: 10000,
     url: "https://red-onion-restaurant-41dbe.web.app/assets/Breakfast/breakfast1.png",
+    category: "New",
+  },
+  {
+    name: "Food 1",
+    content: "Here The Content",
+    price: 10000,
+    url: "https://red-onion-restaurant-41dbe.web.app/assets/Breakfast/breakfast1.png",
+    category: "Popular",
+  },
+  {
+    name: "Food 1",
+    content: "Here The Content",
+    price: 10000,
+    url: "https://red-onion-restaurant-41dbe.web.app/assets/Breakfast/breakfast1.png",
+    category: "",
   },
   {
     name: "Food 1",
@@ -59,18 +74,6 @@ const foodData = [
     price: 10000,
     url: "https://red-onion-restaurant-41dbe.web.app/assets/Breakfast/breakfast1.png",
   },
-  {
-    name: "Food 1",
-    content: "Here The Content",
-    price: 10000,
-    url: "https://red-onion-restaurant-41dbe.web.app/assets/Breakfast/breakfast1.png",
-  },
-  {
-    name: "Food 1",
-    content: "Here The Content",
-    price: 10000,
-    url: "https://red-onion-restaurant-41dbe.web.app/assets/Breakfast/breakfast1.png",
-  }
 ];
 
 export default foodData;
